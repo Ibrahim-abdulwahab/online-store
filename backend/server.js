825303e40f3173458d47fe73884e6329c98a261c
Ibrahim Abdul Wahab Mohammed <112479148+Ibrahim-abdulwahab@users.noreply.github.com>
2025-08-06 00:09:08 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,13 @@ app.use(express.json());
 
 app.use("/api/products",productRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+//if(process.env.NODE_ENV === "production"){
+ //   app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
-    });
-}
+ //   app.get("*",(req,res)=>{
+ //       res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+ //   });
+//}
 
 
 app.listen(PORT,()=>{
